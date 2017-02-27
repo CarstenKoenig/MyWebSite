d12c3f90525193fb87c98540edb8320195ba52d1
Carsten König <Carsten@gettingsharper.de>
2017-02-27 11:32:27 +0100

    render lucid HTML as response

--- app/Main.hs
+++ app/Main.hs
@@ -8,11 +8,16 @@ import Network.Wai (Middleware, Application)
 import Network.Wai.Handler.Warp (run)
 import Network.Wai.Middleware.Static (staticPolicy, addBase)
 
-
 import Control.Monad.Trans
 import Data.Monoid
 import Data.IORef
+
+import qualified Data.Text.Lazy as TL
 import qualified Data.Text as T
+
+import Lucid (Html, renderText)
+import qualified Lucid.Html5 as H
+
 
 data MySession = EmptySession
 data MyAppState = DummyAppState (IORef Int)
@@ -30,8 +35,7 @@ app = do
   -- serve static files from local static folder
   middleware serveStatic
   
-  get root $ do
-    text "Hello World!"
+  get root $ renderHtml helloTemplate
        
   get ("hello" <//> var) $ \name -> do
     DummyAppState ref <- getState
@@ -42,3 +46,10 @@ app = do
 serveStatic :: Middleware
 serveStatic = staticPolicy (addBase "./static")
 
+
+renderHtml :: MonadIO m => Html a -> ActionCtxT ctx m a
+renderHtml = html . TL.toStrict . renderText
+
+helloTemplate :: Html ()
+helloTemplate = do
+  H.h3_ "Hello Lucid"
