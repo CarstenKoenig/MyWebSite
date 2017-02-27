4d84285f37a5e48a55f6f7e34501b22e7c3e35a3
Carsten König <Carsten@gettingsharper.de>
2017-02-27 11:16:34 +0100

    serves static files from ./static

--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,11 @@ module Main where
 import Web.Spock
 import Web.Spock.Config
 
+import Network.Wai (Middleware, Application)
+import Network.Wai.Handler.Warp (run)
+import Network.Wai.Middleware.Static (staticPolicy, addBase)
+
+
 import Control.Monad.Trans
 import Data.Monoid
 import Data.IORef
@@ -12,17 +17,28 @@ import qualified Data.Text as T
 data MySession = EmptySession
 data MyAppState = DummyAppState (IORef Int)
 
+
 main :: IO ()
 main =
     do ref <- newIORef 0
        spockCfg <- defaultSpockCfg EmptySession PCNoDatabase (DummyAppState ref)
        runSpock 8080 (spock spockCfg app)
 
+
 app :: SpockM () MySession MyAppState ()
-app =
-    do get root $
-           text "Hello World!"
-       get ("hello" <//> var) $ \name ->
-           do (DummyAppState ref) <- getState
-              visitorNumber <- liftIO $ atomicModifyIORef' ref $ \i -> (i+1, i+1)
-              text ("Hello " <> name <> ", you are visitor number " <> T.pack (show visitorNumber))
+app = do
+  -- serve static files from local static folder
+  middleware serveStatic
+  
+  get root $ do
+    text "Hello World!"
+       
+  get ("hello" <//> var) $ \name -> do
+    DummyAppState ref <- getState
+    visitorNumber <- liftIO $ atomicModifyIORef' ref $ \i -> (i+1, i+1)
+    text ("Hello " <> name <> ", you are visitor number " <> T.pack (show visitorNumber))
+
+
+serveStatic :: Middleware
+serveStatic = staticPolicy (addBase "./static")
+
