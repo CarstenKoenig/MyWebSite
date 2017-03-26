a66b80428e62336deb47dbf7ecee020cc2039979
Carsten König <carsten@gettingsharper.de>
2017-03-26 13:44:27 +0200

    show category in blogpost-view

--- src/Views/BlogPost.hs
+++ src/Views/BlogPost.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, FlexibleContexts #-}
 
 module Views.BlogPost
   ( BlogPost (..)
@@ -8,19 +8,21 @@ module Views.BlogPost
 import Web.Spock
 import Control.Monad.IO.Class (MonadIO)
 
+import Control.Monad (forM_)
 import Data.Default (def)
 import Data.Maybe (fromMaybe)
 import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.Lazy as TL
+import Data.Time (UTCTime, toGregorian)
+import Data.Time.LocalTime (TimeZone, TimeOfDay (..)
+                           , utcToLocalTime, localTimeOfDay, localDay)
+import Models.Events
 import Text.Blaze (preEscapedText, preEscapedToMarkup)
 import qualified Text.Blaze.Html as TBH
 import Text.Blaze.Html.Renderer.Text (renderHtml)
 import Text.Markdown (Markdown(..), MarkdownSettings(..), markdown)
 import Text.Printf
-import Data.Time (UTCTime, toGregorian)
-import Data.Time.LocalTime (TimeZone, TimeOfDay (..)
-                           , utcToLocalTime, localTimeOfDay, localDay)
 
 
 import Lucid (Html, toHtml, toHtmlRaw)
@@ -41,11 +43,11 @@ page timezone post =
 
 
 pageContent :: TimeZone -> BlogPost -> Html ()
-pageContent timeZone post = do
+pageContent timeZone post =
   H.div_ [ H.class_ "col-sm-8 blog-main" ] $ do
+    H.p_ [ H.class_ "blog-post-meta" ] $ categoryLabels post
     H.h2_ [ H.class_ "blog-post-title" ] $ toHtml . title $ post
-    H.p_ [ H.class_ "blog-post-meta" ] $ do
-      toHtml $ metaInfo timeZone post
+    H.p_ [ H.class_ "blog-post-meta" ] $ toHtml $ metaInfo timeZone post
     toHtmlRaw $ renderBlogPost post
 
 
@@ -57,12 +59,20 @@ metaInfo zone blogPost =
         (y, mn, d)         = toGregorian $ localDay publishedAt
 
 
+categoryLabels :: BlogPost -> Html ()
+categoryLabels blogPost = forM_ (categories blogPost) label
+  where
+    label :: Category -> Html ()
+    label (Category name) =
+      H.span_ [ H.class_ "label label-info" ] (toHtml name)
+
+
 renderBlogPost :: BlogPost -> Text
 renderBlogPost blogPost =
   TL.toStrict . renderHtml $ markdown def
      { msBlockCodeRenderer = renderer
      , msXssProtect        = False
-     } $ md
+     } md
   where
     (Markdown md) = content blogPost
     renderer (Just "math") (src,_) = renderMath src
@@ -73,8 +83,7 @@ renderBlogPost blogPost =
 renderLang :: Maybe Text -> Text -> TBH.Html
 renderLang lang src =
   Sky.formatHtmlBlock Sky.defaultFormatOpts
-  $ highlightAs (fromMaybe "haskell" lang)
-  $ src
+  $ highlightAs (fromMaybe "haskell" lang) src
 
 
 renderFrame :: Text -> TBH.Html
